c7f57c725b7375b4784787d844773201db32dc9a
molottva <molottva@list.ru>
2022-06-15 22:24:33 +0300

    CI success test

--- src/test/java/ru/netology/test/CardDeliveryTest.java
+++ src/test/java/ru/netology/test/CardDeliveryTest.java
@@ -37,94 +37,94 @@ public class CardDeliveryTest {
         success.$x(".//div[@class='notification__content']").should(text("Встреча успешно запланирована на " +
                 user.getDate()));
         success.$x(".//button").click();
-        success.should(visible);
+        success.should(hidden);
     }
 
-//    @Test
-//    public void shouldHappyPathReplanOne() {
-//        UserInfo user = generateUser("ru", 5);
-//        form.$x(".//span[@data-test-id='city']//child::input").val(user.getCity());
-//        form.$x(".//span[@data-test-id='date']//child::input[@class='input__control']").val(user.getDate());
-//        form.$x(".//span[@data-test-id='name']//child::input").val(user.getName());
-//        form.$x(".//span[@data-test-id='phone']//child::input").val(user.getPhone());
-//        form.$x(".//label[@data-test-id='agreement']").click();
-//        form.$x(".//button//ancestor::span[contains(text(), 'Запланировать')]").click();
-//
-//        success.should(visible, ofSeconds(15));
-//        success.$x(".//div[@class='notification__content']").should(text("Встреча успешно запланирована на " +
-//                user.getDate()));
-//        success.$x(".//button").click();
-//        success.should(hidden);
-//
-//        form.$x(".//span[@data-test-id='date']//child::input[@class='input__control']").
-//                val(generateDate(4));
-//        form.$x(".//button//ancestor::span[contains(text(), 'Запланировать')]").click();
-//
-//        replan.should(visible, ofSeconds(15));
-//        replan.$x(".//span[contains(text(), 'Перепланировать')]//ancestor::button").click();
-//        replan.should(hidden);
-//        success.should(visible);
-//        success.$x(".//div[@class='notification__content']").should(text("Встреча успешно запланирована на " +
-//                generateDate(4)));
-//        success.$x(".//button").click();
-//        success.should(hidden);
-//    }
-//
-//    @Test
-//    public void shouldHappyPathReplanTwo() {
-//        UserInfo user = generateUser("ru", 5);
-//        form.$x(".//span[@data-test-id='city']//child::input").val(user.getCity());
-//        form.$x(".//span[@data-test-id='date']//child::input[@class='input__control']").val(user.getDate());
-//        form.$x(".//span[@data-test-id='name']//child::input").val(user.getName());
-//        form.$x(".//span[@data-test-id='phone']//child::input").val(user.getPhone());
-//        form.$x(".//label[@data-test-id='agreement']").click();
-//        form.$x(".//button//ancestor::span[contains(text(), 'Запланировать')]").click();
-//
-//        success.should(visible, ofSeconds(15));
-//        success.$x(".//div[@class='notification__content']").should(text("Встреча успешно запланирована на " +
-//                user.getDate()));
-//        success.$x(".//button").click();
-//        success.should(hidden);
-//
-//        form.$x(".//button//ancestor::span[contains(text(), 'Запланировать')]").click();
-//
-//        replan.should(visible, ofSeconds(15));
-//        replan.$x(".//span[contains(text(), 'Перепланировать')]//ancestor::button").click();
-//        replan.should(hidden);
-//        success.should(visible);
-//        success.$x(".//div[@class='notification__content']").should(text("Встреча успешно запланирована на " +
-//                user.getDate()));
-//        success.$x(".//button").click();
-//        success.should(hidden);
-//    }
-//
-//    @Test
-//    public void shouldHappyPathReplanThree() {
-//        UserInfo user = generateUser("ru", 5);
-//        form.$x(".//span[@data-test-id='city']//child::input").val(user.getCity());
-//        form.$x(".//span[@data-test-id='date']//child::input[@class='input__control']").val(user.getDate());
-//        form.$x(".//span[@data-test-id='name']//child::input").val(user.getName());
-//        form.$x(".//span[@data-test-id='phone']//child::input").val(user.getPhone());
-//        form.$x(".//label[@data-test-id='agreement']").click();
-//        form.$x(".//button//ancestor::span[contains(text(), 'Запланировать')]").click();
-//
-//        success.should(visible, ofSeconds(15));
-//        success.$x(".//div[@class='notification__content']").should(text("Встреча успешно запланирована на " +
-//                user.getDate()));
-//        success.$x(".//button").click();
-//        success.should(hidden);
-//
-//        form.$x(".//span[@data-test-id='date']//child::input[@class='input__control']").
-//                val(generateDate(6));
-//        form.$x(".//button//ancestor::span[contains(text(), 'Запланировать')]").click();
-//
-//        replan.should(visible, ofSeconds(15));
-//        replan.$x(".//span[contains(text(), 'Перепланировать')]//ancestor::button").click();
-//        replan.should(hidden);
-//        success.should(visible);
-//        success.$x(".//div[@class='notification__content']").should(text("Встреча успешно запланирована на " +
-//                generateDate(6)));
-//        success.$x(".//button").click();
-//        success.should(hidden);
-//    }
+    @Test
+    public void shouldHappyPathReplanOne() {
+        UserInfo user = generateUser("ru", 5);
+        form.$x(".//span[@data-test-id='city']//child::input").val(user.getCity());
+        form.$x(".//span[@data-test-id='date']//child::input[@class='input__control']").val(user.getDate());
+        form.$x(".//span[@data-test-id='name']//child::input").val(user.getName());
+        form.$x(".//span[@data-test-id='phone']//child::input").val(user.getPhone());
+        form.$x(".//label[@data-test-id='agreement']").click();
+        form.$x(".//button//ancestor::span[contains(text(), 'Запланировать')]").click();
+
+        success.should(visible, ofSeconds(15));
+        success.$x(".//div[@class='notification__content']").should(text("Встреча успешно запланирована на " +
+                user.getDate()));
+        success.$x(".//button").click();
+        success.should(hidden);
+
+        form.$x(".//span[@data-test-id='date']//child::input[@class='input__control']").
+                val(generateDate(4));
+        form.$x(".//button//ancestor::span[contains(text(), 'Запланировать')]").click();
+
+        replan.should(visible, ofSeconds(15));
+        replan.$x(".//span[contains(text(), 'Перепланировать')]//ancestor::button").click();
+        replan.should(hidden);
+        success.should(visible);
+        success.$x(".//div[@class='notification__content']").should(text("Встреча успешно запланирована на " +
+                generateDate(4)));
+        success.$x(".//button").click();
+        success.should(hidden);
+    }
+
+    @Test
+    public void shouldHappyPathReplanTwo() {
+        UserInfo user = generateUser("ru", 5);
+        form.$x(".//span[@data-test-id='city']//child::input").val(user.getCity());
+        form.$x(".//span[@data-test-id='date']//child::input[@class='input__control']").val(user.getDate());
+        form.$x(".//span[@data-test-id='name']//child::input").val(user.getName());
+        form.$x(".//span[@data-test-id='phone']//child::input").val(user.getPhone());
+        form.$x(".//label[@data-test-id='agreement']").click();
+        form.$x(".//button//ancestor::span[contains(text(), 'Запланировать')]").click();
+
+        success.should(visible, ofSeconds(15));
+        success.$x(".//div[@class='notification__content']").should(text("Встреча успешно запланирована на " +
+                user.getDate()));
+        success.$x(".//button").click();
+        success.should(hidden);
+
+        form.$x(".//button//ancestor::span[contains(text(), 'Запланировать')]").click();
+
+        replan.should(visible, ofSeconds(15));
+        replan.$x(".//span[contains(text(), 'Перепланировать')]//ancestor::button").click();
+        replan.should(hidden);
+        success.should(visible);
+        success.$x(".//div[@class='notification__content']").should(text("Встреча успешно запланирована на " +
+                user.getDate()));
+        success.$x(".//button").click();
+        success.should(hidden);
+    }
+
+    @Test
+    public void shouldHappyPathReplanThree() {
+        UserInfo user = generateUser("ru", 5);
+        form.$x(".//span[@data-test-id='city']//child::input").val(user.getCity());
+        form.$x(".//span[@data-test-id='date']//child::input[@class='input__control']").val(user.getDate());
+        form.$x(".//span[@data-test-id='name']//child::input").val(user.getName());
+        form.$x(".//span[@data-test-id='phone']//child::input").val(user.getPhone());
+        form.$x(".//label[@data-test-id='agreement']").click();
+        form.$x(".//button//ancestor::span[contains(text(), 'Запланировать')]").click();
+
+        success.should(visible, ofSeconds(15));
+        success.$x(".//div[@class='notification__content']").should(text("Встреча успешно запланирована на " +
+                user.getDate()));
+        success.$x(".//button").click();
+        success.should(hidden);
+
+        form.$x(".//span[@data-test-id='date']//child::input[@class='input__control']").
+                val(generateDate(6));
+        form.$x(".//button//ancestor::span[contains(text(), 'Запланировать')]").click();
+
+        replan.should(visible, ofSeconds(15));
+        replan.$x(".//span[contains(text(), 'Перепланировать')]//ancestor::button").click();
+        replan.should(hidden);
+        success.should(visible);
+        success.$x(".//div[@class='notification__content']").should(text("Встреча успешно запланирована на " +
+                generateDate(6)));
+        success.$x(".//button").click();
+        success.should(hidden);
+    }
 }
